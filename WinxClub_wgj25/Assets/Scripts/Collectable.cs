cac0f101451d6fefb5e88356e657de5228a152d7
Amanda <amanda.lrib@gmail.com>
2025-08-10 18:07:14 -0300

    fix: items not collecting

--- WinxClub_wgj25/Assets/Scripts/Collectable.cs
+++ WinxClub_wgj25/Assets/Scripts/Collectable.cs
@@ -4,9 +4,11 @@ public class Collectable : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.CompareTag("Caprichoso") || !other.CompareTag("Garantido")) return;
-        Debug.Log("Collected");
-        GameManager.Instance.IncreaseCollectableCount();
-        gameObject.SetActive(false);
+        if (other.CompareTag("Caprichoso") || other.CompareTag("Garantido"))
+        {
+            Debug.Log("Collected");
+            GameManager.Instance.IncreaseCollectableCount();
+            gameObject.SetActive(false);
+        }
     }
 }
