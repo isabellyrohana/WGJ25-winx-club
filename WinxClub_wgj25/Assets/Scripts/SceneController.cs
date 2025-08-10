de8586dbee94186904da9c8e292f2f7368fcf6cd
Amanda <amanda.lrib@gmail.com>
2025-08-10 20:51:37 -0300

    feature: create start menu

---
+++ WinxClub_wgj25/Assets/Scripts/SceneController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+     public static SceneController Instance;
+     
+     private void Awake()
+     {
+         if (Instance != null && Instance != this)
+         {
+             Destroy(gameObject);
+         }
+         else
+         {
+             Instance = this;
+         }
+        
+         DontDestroyOnLoad(gameObject);
+     }
+     
+    public void LoadNextScene()
+    {
+        if (SceneManager.GetActiveScene().buildIndex + 1 > SceneManager.sceneCountInBuildSettings)
+        {
+            Debug.LogWarning("Última cena já está carregada.");
+            return;
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadPreviousScene()
+    {
+        if (SceneManager.GetActiveScene().buildIndex - 1 < 0)
+        {
+            Debug.LogWarning("Primeira cena já está carregada.");
+            return;
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Debug.LogWarning("QuitGame");
+        Application.Quit();
+    }
+}
